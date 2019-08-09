5e767aa4694a5b6c685dfb11de1c8db57ff002c6
Q. Wang <qian.wang@live.com>
2019-08-09 14:45:29 -0700

    Add control flow and docstrings to instrmap.py to make it an executable task

--- instrmap.py
+++ instrmap.py
@@ -1,11 +1,43 @@
 #!/usr/bin/env python3
+"""
+instrmap.py
 
+Usage:
+
+instrmap.py A01_cl.evt [usrbpix=morebadpix.fits] [prefix=prefix] [dryrun=yes]
+
+Create an instrument map for a specific observation. The user can specify
+additional bad pixels by giving files that have BADPIX extension(s) using the
+usrbpix= argument. Multiple files can be given, separated by commas (with no
+space in the argument). Bad pixels from CALDB will always be applied.
+
+The prefix= argument lets user add a file name prefix for the output file.
+This allows the output to be written to any specified path. If a directory is
+intended, it must end with '/'.
+
+If dryrun=yes the task will stop after the bad pixel files have been checked,
+before the instrument map is calculated.
+"""
 import sys
 import os
 import datetime
 import astropy.io.fits as pf
 import numpy as np
 import scipy.ndimage
+
+
+class CaldbIndx():
+    def __init__(self, caldbindxpath):
+        self.indxpath = caldbindxpath
+        self.indxfits = pf.open(caldbindxpath)
+        self.version = self.indxfits['CIF'].header['CALDBVER']
+
+
+def fpm_parse(keyword):
+    if keyword not in ('FPMA', 'FPMB'):
+        return False
+    else:
+        return keyword[-1]
 
 
 DETNAM = {'DET0': 0, 'DET1': 1, 'DET2': 2, 'DET3': 3}
@@ -189,23 +221,142 @@ def apply_badpix(instrmap, bpixfile, pixmap, detnum):
 
 
 if __name__ == '__main__':
-    # Inputs
-    # FITS file(s) with BADPIX extensions
-    bpixfiles = ['bp.fits', '_cl.evt', 'usrbp.fits']
-    # Output will be named outprefix_A.fits and/or outprefix_B.fits
-    outprefix = 'test'
 
+    ERR_CALDB_NOTFOUND = """
+Error: checking for calibration files in $CALDB/data/nustar, but cannot find
+caldb.indx in this CALDB location. Is the $CALDB path correct?
+
+$CALDB = {caldbpath}
+
+Cannot find the file
+
+{caldbinxpath}
+"""
+
+    ERR_BPIXFILE_NOTFOUND = """
+Error: bad pixel file not found, skipping:
+{file}
+"""
+
+    if len(sys.argv) == 1:
+        print(__doc__)
+        sys.exit(0)
+
+    obsinfofile = sys.argv[1]
+
+    keywords = {
+        'usrbpix': None,
+        'prefix': '',
+        'dryrun': ''
+    }
+
+    for _ in sys.argv[1:]:
+        arg = _.split('=')
+        if arg[0] in keywords:
+            keywords[arg[0]] = arg[1]
+
+    scriptname = os.path.basename(__file__)
+    print('Running %s. Performing checks...' % scriptname)
+
+    halt = False
+
+    # Inputs File with observation info must exist. Typically this is the
+    # event file itself, but this program needs only the information in its
+    # FITS header.
+    require_hdr_keywords = ['INSTRUME']
+    if not os.path.exists(obsinfofile):
+        print('Error: %s does not exist.' % obsinfofile)
+        halt = True
+        sys.exit(1)  # No further checks if the main file doesn't exist
+
+    evtfile = pf.open(obsinfofile)
+    if 'EVENTS' not in evtfile:
+        print('Error: EVENTS extension not found in %s.' % obsinfofile)
+        halt = True
+        sys.exit(1)
+
+    evthdr = evtfile['EVENTS'].header
+    for _ in require_hdr_keywords:
+        if _ not in evthdr:
+            halt = True
+            print('Error: required keyword %s not in %s' % (_, obsinfofile))
+
+    ab = fpm_parse(evthdr['INSTRUME'])
+    if ab is False:
+        halt = True
+        print('Invalid INSTRUME keyword value: %s' % evthdr['INSTRUME'])
+
+    if halt:
+        sys.exit(1)
+
+    # FITS file(s) with BADPIX extensions
+    bpixfiles = [obsinfofile]
+
+    if keywords['usrbpix'] is not None:
+        for _ in keywords['usrbpix'].split(','):
+            if os.path.exists(_):
+                bpixfiles.append(_)
+            else:
+                print(ERR_BPIXFILE_NOTFOUND.format(file=_))
+
+    # Output will be named outprefix_A.fits and/or outprefix_B.fits
+    outprefix = keywords['prefix']
+
+    outfilename = outprefix + 'newinstrmap' + ab + '.fits'
+    if os.path.exists(outfilename):
+        print('Error: output file exists!')
+        halt = True
+
+    # Check CALDB environment
+    try:
+        caldb = os.environ['CALDB']
+    except KeyError:
+        halt = True
+        print('Error: environment variable CALDB is undefined.')
+    else:
+        # Check if it is valid by looking for a file in nustar's fpm data.
+        # Doesn't guarantee the other files are OK, but will warn if CALDB
+        # path is incorrect altogether.
+        caldbinxpath = '$CALDB/data/nustar/fpm/caldb.indx'
+        if not os.path.exists(caldbinxpath.replace('$CALDB', caldb)):
+            halt = True
+            print(ERR_CALDB_NOTFOUND.format(
+                caldbpath=caldb, caldbinxpath=caldbinxpath))
+        else:
+            print('$CALDB=%s' % caldb)
+            caldbinx = CaldbIndx(caldbinxpath.replace('$CALDB', caldb))
+            print('CALDB version: %s' % caldbinx.version)
+
+    if halt:
+        print('%s did not complete. See error messages.' % scriptname)
+        sys.exit(1)
+
+    print('Preliminary checks complete, proceeding...')
+
+    print('Creating instrument maps for FPM%s for observation %s' % (
+        ab, obsinfofile))
+
+    caldbbpixpath = ('%s/data/nustar/fpm/bcf/badpix/'
+                     'nu%sbadpix20100101v002.fits') % (caldb, ab)
+    bpixfiles.append(caldbbpixpath)
+
+    print('Collecting bad pixel lists...')
     bpixexts = get_badpix_exts(bpixfiles)
-    ab = 'A'   # Get this from INSTRUME keyword
+
+    # Stop here if doing a dry run
+    if keywords['dryrun'] == 'yes':
+        sys.exit(0)
+
+    print('Loading CALDB instrmap...')
     instrmap, header = get_caldb_instrmap(ab)
+    print('Loading CALDB pixpos...')
     pixmap, detnum = get_caldb_pixpos(ab)
 
-    caldb = os.environ['CALDB']
-    bpixpath = ('%s/data/nustar/fpm/bcf/badpix/'
-                'nu%sbadpix20100101v002.fits') % (caldb, ab)
+    print('Applying bad pixels lists for FPM%s...' % ab)
 
-    pf.HDUList(pf.PrimaryHDU(instrmap, header=header)).writeto(
-    outdir + 'newinstrmap' + ab + '.fits')
+    pf.HDUList(
+        pf.PrimaryHDU(instrmap, header=header)
+    ).writeto(outfilename)
 
 
 """
