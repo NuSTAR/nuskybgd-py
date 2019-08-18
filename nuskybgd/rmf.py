e71dde0823d6090d434ab910d76c934a66daa8ab
Q. Wang <qian.wang@live.com>
2019-08-18 04:48:52 -0600

    Add weighted detabs function, refactor add abs to rmf function

--- nuskybgd/rmf.py
+++ nuskybgd/rmf.py
@@ -1,5 +1,6 @@
 import astropy.io.fits as pf
 import os
+import numpy as np
 from . import caldb
 from . import util
 
@@ -26,7 +27,7 @@ def add_abs_hdu(detabshdu, rmfhdu):
 
     try:
         absmatrix = (rmfhdu.data['MATRIX'] *
-                         detabshdu.data['DETABS'])
+                     detabshdu.data['DETABS'])
     except KeyError:
         raise KeyError('MATRIX and DETABS columns are required.')
 
@@ -42,6 +43,78 @@ def add_abs_fits(detabsfile, rmffile):
     pass
 
 
+def detabs_hdu(abshdus, weights):
+    """
+    Coadd absorption spectrum of the detectors using the given weights. The
+    weights are normalized to unity before proceeding.
+
+    Example uses:
+
+    - Get detector absorption weighed by area inside a source region.
+    - Get absorption of a single detector by giving it 100% weight.
+
+    Inputs:
+
+    abshdus -- HDUList (e.g. CALDB DETABS FITS file)
+    weights -- A list of 4 values
+
+    Outputs:
+
+    BinTableHDU 'DETABS' containing absorption data
+    """
+    if not ((isinstance(weights, list) or
+             isinstance(weights, np.ndarray)) and
+            len(weights) == 4):
+        raise ValueError('weights must be a length-4 array')
+    if not isinstance(abshdus, pf.hdu.hdulist.HDUList):
+        raise ValueError('detabshdus must be a HDUList object')
+
+    weight_sum = np.sum(weights)
+
+    if not (weight_sum > 0 and np.min(weights) >= 0):
+        raise ValueError('weights must be >= 0 and sum to > 0')
+
+    weights = np.float64(weights) / weight_sum
+
+    # Create new HDU for the results
+    results = abshdus[1].copy()
+    results.data['DETABS'] *= 0.0
+
+    for idet in range(4):
+        if weights[idet] > 0:
+            results.data['DETABS'] += (
+                weights[idet] * abshdus[idet + 1].data['DETABS'])
+
+    util.hdu_timestamp_write(results)
+    msg = 'Weighted DETABS: %.8e %.8e %.8e %.8e' % tuple(weights)
+    util.hdu_history_write(results, msg)
+
+    return results
+
+
+def add_weighted_abs(abshdus, weights, rmfhdu):
+    """
+    Add absorption to an RMF, using given weights for the detectors.
+
+    **The input 'rmfhdu' is modified in-place.**
+
+    Inputs:
+
+    abshdus -- HDUList (e.g. CALDB DETABS FITS file)
+    weights -- A list of 4 values
+    rmfhdu -- BinTableHDU with RMF data
+    """
+    weighted_abs = detabs_hdu(abshdus, weights)
+
+    # Modifies rmffh['MATRIX'] in place
+    add_abs_hdu(weighted_abs, rmfhdu)
+
+    msg = (
+        'RMF includes DETABS weights: '
+        '%.8e %.8e %.8e %.8e' % tuple(weights)
+    )
+    util.hdu_history_write(rmfhdu, msg)
+
 
 def make_absrmf(evtfile, outfile,
                 rmffile='CALDB', detabsfile='CALDB',
@@ -53,7 +126,11 @@ def make_absrmf(evtfile, outfile,
     cal = caldb.CalDB(os.environ['CALDB'])
 
     outfilename = outfile + '%d%s.rmf'
+
     # Check if output files exist
+    dirname = os.path.dirname(outfile)
+    if dirname != '':
+        os.makedirs(dirname, exist_ok=True)
     halt = False
     for idet in range(4):
         if os.path.exists(outfilename % (idet, ab)):
@@ -94,17 +171,12 @@ def make_absrmf(evtfile, outfile,
     for idet in range(4):
         rmffh = pf.open(rmflist[idet])
         detabsfh = pf.open(detabslist[idet])
+        weights = [0.0] * 4
+        weights[idet] = 1.0
 
         # Modifies rmffh['MATRIX'] in place
-        add_abs_hdu(detabsfh[idet + 1], rmffh['MATRIX'])
+        add_weighted_abs(detabsfh, weights, rmffh['MATRIX'])
 
-        # Create the output file
-        history_msg = (
-            'Modified RMF which includes DETABS, using files %s and %s' % (
-                rmflist[idet], detabslist[idet]
-            )
-        )
-        util.hdu_history_write(rmffh['MATRIX'], history_msg)
         util.fits_write(rmffh, outfilename % (idet, ab),
                                  overwrite=overwrite)
 
