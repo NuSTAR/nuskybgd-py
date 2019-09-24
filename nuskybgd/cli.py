ae511a7ee0aa93123d9351afa3f444942ef6272e
Q. Wang <qian.wang@live.com>
2019-09-23 17:31:17 -0700

    Refactor projinitbgds

--- nuskybgd/cli.py
+++ nuskybgd/cli.py
@@ -5,6 +5,7 @@ script, by passing an arguments list to them in lieu of sys.argv.
 """
 from . import conf
 
+
 def run(args=[]):
     """
     Run nuskybgd tasks.
@@ -17,6 +18,7 @@ def run(args=[]):
     tasks = {
         'absrmf': absrmf,
         'fitab': fitab,
+        'projinitbgds': projinitbgds,
         'projobs': projobs
     }
 
@@ -242,6 +244,178 @@ Sample bgdinfo.json:
         numodel.save_xcm()
     else:
         numodel.save_xcm(prefix=keywords['savefile'].strip())
+
+    return 0
+
+
+def projinitbgds(args=[]):
+    """
+    Project the instrument map onto sky coordinates to get the projected
+    detector mask images and background aperture images.
+
+    Usage:
+
+    nuskybgd projinitbgds refimg=flux.fits out=output.fits \\
+        mod=[A,B] det=[1,2,3,4] chipmap=chipmap.fits aspect=aspect.fits
+
+    Example:
+
+    nuskybgd projinitbgds refimg=imA4to25keV.fits out=bgdapA.fits \\
+        mod=A det=1234 chipmap=newinstrmapA.fits aspect=aspecthistA.fits
+
+    The output file bgdapA.fits has the background aperture image in sky
+    coordinates. A series of files showing the projected detector masks for
+    each detector specified by det= will also be created, named
+    det[0-3]Aim.fits.
+    """
+    if conf.block() is True:
+        return 1
+
+    if len(args) == 1:
+        print(projinitbgds.__doc__)
+        return 0
+
+    import os
+    import sys
+    import numpy as np
+    import astropy.io.fits as pf
+    from .image import (
+        get_det_mask, get_aspect_hist_image, get_aspect_hist_peak,
+        get_aperture_image, transform_image, calc_pa_to_arot
+    )
+    opts = {}
+    keys = ('refimg', 'out', 'det', 'mod', 'chipmap', 'aspect')
+    # TODO: validate input chipmap and aspect files
+    for _ in args[1:]:
+        pars = _.split('=')
+        if pars[0] in keys:
+            opts[pars[0]] = pars[1]
+
+    for _ in keys:
+        if _ not in opts:
+            print('Missing %s= keyword' % _)
+            return 1
+
+    try:
+        refimg = pf.open(opts['refimg'])
+    except FileNotFoundError:
+        print('refimg file not found')
+        return 1
+
+    try:
+        hdr = refimg[0].header
+        pa = hdr['PA_PNT'] + 1.0
+        required = ('CTYPE1', 'CTYPE2', 'CRPIX1', 'CRPIX2',
+                    'CRVAL1', 'CRVAL2', 'CDELT1', 'CDELT2')
+        for _ in required:
+            if _ not in hdr:
+                raise KeyError('WCS info missing from header' % _)
+        if hdr['NAXIS1'] != 1000 or hdr['NAXIS2'] != 1000:
+            raise ValueError('Unexpected image size (not 1000x1000)')
+    except KeyError:
+        print('Required information not found in header')
+        return 1
+    except ValueError:
+        print('Unexpected refimg format')
+        return 1
+
+    if opts['mod'] not in ('A', 'B'):
+        print('mod= must be set to A or B')
+        return 1
+
+    det_input = []
+    for _ in '1234':
+        if _ in opts['det']:
+            det_input.append(int(_))
+    if len(det_input) == 0:
+        print('No valid detector number specified.')
+        return 1
+
+    auxildir = conf._AUX_DIR
+
+    if opts['out'][0] != '!':
+        halt = False
+        if os.path.exists(opts['out']):
+            print('Output file %s exists.' % opts['out'])
+            halt = True
+        for idet in np.arange(4):
+            _ = 'det%d%sim.fits' % (idet, opts['mod'])
+            if os.path.exists(_):
+                print('Output file %s exists.' % _)
+                halt = True
+        if halt:
+            return 1
+
+    print('Using auxiliary data in %s' % auxildir)
+
+    detmask = get_det_mask(opts['chipmap'], det_input)
+    apim = get_aperture_image(opts['mod'])
+    posim, pos_xoff, pos_yoff = get_aspect_hist_image(opts['aspect'])
+    totalexp = np.sum(posim)
+
+    arot = calc_pa_to_arot(pa)
+
+    asppeakx, asppeaky = get_aspect_hist_peak(posim, pos_xoff, pos_yoff)
+
+    xgrid, ygrid = np.meshgrid(np.arange(1000),
+                               np.arange(1000), indexing='xy')
+
+    detxa = np.int32(350 + np.around(- np.cos(arot) * (xgrid - asppeakx) +
+                                     np.sin(arot) * (ygrid - asppeaky)))
+    detya = np.int32(350 + np.around(np.sin(arot) * (xgrid - asppeakx) +
+                                     np.cos(arot) * (ygrid - asppeaky)))
+    nudge = [2.7, 0.8]
+    nudge = [nudge[0] * np.cos(-arot) - nudge[1] * np.sin(-arot),
+             nudge[0] * np.sin(-arot) + nudge[1] * np.cos(-arot)]
+
+    imval = detmask / totalexp  # Normalize it, since convolving with posim
+    apval = apim / totalexp        # multiplies it by sum(posim)
+
+    refimap = np.zeros((1000, 1000), dtype=np.float64)
+    refimi = np.zeros((len(det_input), 1000, 1000), dtype=np.float64)
+
+    print('Rotating instrument map (PA = %f, %f radians)' % (pa, arot))
+    for i in np.arange(1000):
+        for j in np.arange(1000):
+            detx = detxa[j, i]
+            dety = detya[j, i]
+            for idet_input in range(len(det_input)):
+                if ((0 <= detx < 360) and (0 <= dety < 360) and
+                        (detmask[idet_input, dety, detx] > 0)):
+                    refimap[j, i] = apval[dety, detx]
+                    refimi[idet_input, j, i] = imval[idet_input, dety, detx]
+
+    bgdimap = np.zeros((1000, 1000), dtype=np.float64)
+    bgdimi = np.zeros((len(det_input), 1000, 1000), dtype=np.float64)
+
+    inx = np.where(posim > 0)
+    progress_total = len(inx[0])
+    progress = 0
+    print('Convolving instrument map with aspect solution...')
+    x_offset = pos_xoff - asppeakx - nudge[0]
+    y_offset = pos_yoff - asppeaky - nudge[1]
+    for x, y in zip(inx[1], inx[0]):
+        bgdimap += posim[y, x] * transform_image(
+            refimap, x + x_offset, y + y_offset, 0)
+        for idet_input in range(len(det_input)):
+            bgdimi[idet_input] += posim[y, x] * transform_image(
+                refimi[idet_input], x + x_offset, y + y_offset, 0)
+        progress += 1
+        sys.stdout.write('\r %d/%d positions' % (progress, progress_total))
+        sys.stdout.flush()
+
+    print('\nDone.')
+
+    outfile = pf.HDUList([pf.PrimaryHDU(bgdimap, header=hdr)])
+
+    # Write background aperture image
+    outfile.writeto(opts['out'], overwrite=True)
+
+    # Write detector mask images
+    for idet_input in range(len(det_input)):
+        pf.HDUList([pf.PrimaryHDU(bgdimi[idet_input], header=hdr)]).writeto(
+            'det%d%sim.fits' % (det_input[idet_input] - 1, opts['mod']),
+            overwrite=True)  # For det number, input 1-4 -> output 0-3
 
     return 0
 
